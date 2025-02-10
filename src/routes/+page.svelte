f4a4b10f6a3d7dc59f26b9719e2be59dce7aa549
PrudhviBoggavarapu <codingpabs@gmail.com>
2025-02-09 22:16:08 -0600

    Adding the still waiting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,31 +1,27 @@
 <script>
 	import Question from '$lib/Questions/question.svelte';
 	import { questions } from '$lib/types';
-    import { selected_answers } from '$lib/stores';
+	import { selected_answers } from '$lib/stores';
 	import { Button } from '$lib/components/ui/button';
-    function submit() {
-        console.log($selected_answers);
-        let correct_answers = 0;
-        for (let i = 0; i < questions.length; i++) {
-            if ($selected_answers[i] === questions[i].correct_answer) {
-                correct_answers++;
-            }
-        }
-        console.log(correct_answers);
-        return correct_answers;
-    }
-
+	function submit() {
+		console.log($selected_answers);
+		let correct_answers = 0;
+		for (let i = 0; i < questions.length; i++) {
+			if ($selected_answers[i] === questions[i].correct_answer) {
+				correct_answers++;
+			}
+		}
+		console.log(correct_answers);
+		return correct_answers;
+	}
 </script>
 
-<main class="p-4 overflow-y-auto ">
-
-<div class="p-4 overflow-y-auto space-y-8">
-    {#each questions as question, i}
+<main class="overflow-y-auto p-4">
+	<div class="space-y-8 overflow-y-auto p-4">
+		<!-- {#each questions as question, i}
         <Question question={question} question_number={i}   />
     {/each}
-    <Button on:click={submit}>Submit</Button>
-</div>
+    <Button on:click={submit}>Submit</Button> -->
+		<h1>Still Baking....</h1>
+	</div>
 </main>
-
-
-
